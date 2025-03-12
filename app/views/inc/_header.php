07fd1a4905497feefc4ea64f560ecdda02a80080
ikerguerra <152733086+ikerguerra@users.noreply.github.com>
2025-03-12 13:19:42 +0100

    Diseño CSS Productos

--- app/views/inc/_header.php
+++ app/views/inc/_header.php
@@ -9,6 +9,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo MIURL;?>public/assets/css/custom.css" id="theme-color">
     <link rel="stylesheet" href="<?php echo MIURL;?>public/assets/css/styles.css" id="theme-color">
 </head>
--- app/views/pages/productos.php
+++ app/views/pages/productos.php
@@ -2,7 +2,34 @@
 <?php include '../inc/_header.php' ?>
 <?php include '../inc/_navigation.php' ?>
 
-<h2>Productos disponibles</h2>
-<p>Estas son todas nuestras zapatillas</p>
+<!-- Productos -->
+<main class="py-5 bg-dark section-angle top-left bottom-left" id="productos">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-6 mx-auto text-center">
+                <h2 class="heading-black">Productos disponibles</h2>
+                <p class="text-muted lead">Estas son todas nuestras zapas lokete</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+                <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="card h-100">
+                        <a href="#">
+                            <picture class="h-100">
+                                <img class="card-img-top img-fluid"
+                                    src="<?php echo MIURL;?>public/assets/img/adidas-campus.webp" alt="Blog 1">
+                            </picture>
+                        </a>
+                        <div class="card-body d-flex flex-column align-items-start">
+                            <a href="#" class="card-title mb-2">
+                                <h5>producto</h5>
+                            </a>
+                            <p class="text-muted small-xl mt-auto">120 €</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</main>
 
 <?php include '../inc/_footer.php' ?>
